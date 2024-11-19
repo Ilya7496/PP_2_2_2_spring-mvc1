07daf4666a8b2b7f8ab4c3183b251985bb55412b
ilyamikhailov <79221803601me@gmail.com>
2024-11-19 23:00:11 +0300

    Изменения

---
+++ src/main/java/web/Service/CarService.java
@@ -0,0 +1,10 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CarService {
+    List<Car> getCars(Optional<Integer> carsQuantity);
+}
---
+++ src/main/java/web/Service/CarServiceImpl.java
@@ -0,0 +1,28 @@
+package web.Service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CarServiceImpl {
+    private final List<Car> cars = Arrays.asList(
+            new Car(1, "AUDI", 1996),
+            new Car(2, "BMW", 1997),
+            new Car(3, "TOYOTA", 1997),
+            new Car(4, "MERCEDES", 1995),
+            new Car(5, "FORD", 1995));
+
+    public List<Car> getCars(Optional<Integer> carsQuantity) {
+        return carsQuantity.map(i -> {
+            if (i <= cars.size()) {
+                return cars.subList(0, i);
+            } else {
+                return cars;
+            }
+        }).orElseGet(() -> cars);
+    }
+}
